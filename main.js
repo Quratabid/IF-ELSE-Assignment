56580c50c872f018def2e1106343259ebf53e189
Quratabid <162566815+Quratabid@users.noreply.github.com>
2024-03-08 01:00:20 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+"use strict";
+// let percentage = 80;
+// if (percentage >= 80 && percentage <= 100){
+//     console.log("A-one grade");
+// }
+// else if(percentage >= 70 && percentage <= 79){
+//     console.log("A grade");
+// }
+// else if(percentage >= 60 && percentage <= 69){
+//     console.log("B grade");
+// }
+// else if(percentage >= 50 && percentage <= 59){
+//     console.log("C grade");
+// }
+// else if(percentage >= 40 && percentage <= 49){
+//     console.log("D grade");
+// }
+// else{
+//     console.log("fail");
+// }
+let marks = 40;
+if (marks >= 60 && marks <= 69) {
+    console.log("B");
+}
+else if (marks >= 50 && marks <= 59) {
+    console.log("c");
+}
+else {
+    console.log("fail");
+}
---
+++ main.ts
@@ -0,0 +1,22 @@
+// let percentage = 80;
+
+// if (percentage >= 80 && percentage <= 100){
+//     console.log("A-one grade");
+// }
+// else if(percentage >= 70 && percentage <= 79){
+//     console.log("A grade");
+// }
+// else if(percentage >= 60 && percentage <= 69){
+//     console.log("B grade");
+// }
+// else if(percentage >= 50 && percentage <= 59){
+//     console.log("C grade");
+// }
+// else if(percentage >= 40 && percentage <= 49){
+//     console.log("D grade");
+// }
+// else{
+//     console.log("fail");
+// }
+
+
